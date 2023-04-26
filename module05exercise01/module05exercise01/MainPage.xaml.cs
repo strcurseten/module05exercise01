99ff076252eec20f7275a474b129223148f53d2d
ten <101978567+strcurseten@users.noreply.github.com>
2023-04-26 19:01:23 +0800

    first commit

---
+++ module05exercise01/module05exercise01/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace module05exercise01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void stepperpage(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new stepperpage());
+        }
+    }
+}
---
+++ module05exercise01/module05exercise01/stepperpage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace module05exercise01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class stepperpage : ContentPage
+    {
+        public stepperpage()
+        {
+            InitializeComponent();
+        }
+
+        void OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            _rotatingLabel.Rotation = value;
+            _displayLabel.Text = string.Format("The Stepper value is {0}", value);
+        }
+    }
+}
